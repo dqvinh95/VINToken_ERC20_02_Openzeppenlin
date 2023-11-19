eda7e4a9a08e2f55371dbde0c833783f2632a2a0
dqvinh95 <31183124+dqvinh95@users.noreply.github.com>
2023-11-19 11:31:39 +0700

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  network: {
+    bsctest: {
+      url: "https://data-seed-prebsc-2-s2.binance.org:8545",
+      accounts: [process.env.PRIV_KEY]
+    }
+  }
+};
+
+
